81ec650d3bab62db0d966ed6497cc3db6f1abac2
Jose Knox Borquez <jose.borquezg@sansano.usm.cl>
2020-05-06 22:55:24 -0400

    Stage 1 Ready v1.0

--- Etapa1/Joystick.java
+++ Etapa1/Joystick.java
@@ -1,48 +1,26 @@
 public class Joystick {
-   // FIELDS
+   /* FIELDS */
    private float vf, hf;
 
+   /* Constructor */
    public Joystick () {
       vf=0.0f;
       hf=0.0f;
    }
 
-   // Methods
-   /** 
-    * Determina una posicion vertical del joystick segun 
-    * el parametro tipo float Value.
-    * @param Value (float): angulo de posicion
-    */
-   public void setVerPos(float Value){
-      vf = Value;
+   /* Methods */
+   public void setVerPos(float f){
+      vf = f;
    }
-   /** 
-    * Determina una posicion horizontal del joystick segun 
-    * el parametro tipo float Value.
-    * @param Value (float): angulo de posicion
-    */
-   public void setHorPos(float Value){
-      hf = Value;
+   public void setHorPos(float f){
+      hf = f;      
    } 
-   /** 
-    * Obtiene la posicion vertical del joystick.
-    * @return float: angulo de posicion vertical
-    */
    public float getVerPos() {
-     return vf;
-   } 
-   /** 
-    * Obtiene la posicion horizontal del joystick.
-    * @return float: angulo de posicion horizontal
-    */
+      return vf;
+   }
    public float getHorPos() {
       return hf;
-   } 
-   /** 
-    * Convierte las posiciones hor. y vert. del joystick
-    * en datos tipo String.
-    * @return String: angulos de posicion
-    */
+   }
    public String toString() {
       return hf+","+vf;
    }
--- Etapa1/Stage1Test.java
+++ Etapa1/Stage1Test.java
@@ -3,18 +3,19 @@ import java.util.Scanner;
 import java.util.Locale;
 
 public class Stage1Test  {
-   public static void main (String[] arg) throws IOException {
-      Locale.setDefault(Locale.US);  // to read number in US format, like 1.5 (not like 1,5)
-      Scanner in = new Scanner(new File(arg[0]));
-      float time = 0.0f;
-      Joystick l_Joy = new Joystick();
-      Joystick r_Joy = new Joystick();
-      Operator operator = new Operator(in, l_Joy, r_Joy);
-      
-      System.out.println("Time\tJIH\tJIV\tJDH\tJDV");
-      while(operator.takeAction(time)) {
-         System.out.printf("%.1f,\t%s, %s%n",time,l_Joy.toString(),r_Joy.toString());
-         time+=0.1;
-      }
-   }
+    public static void main (final String[] arg) throws IOException {
+        Locale.setDefault(Locale.US);  // to read number in US format, like 1.5 (not like 1,5)
+        final Scanner in = new Scanner(new File(arg[0]));
+        
+        // Time init
+        float time = 0.0f;
+        
+        final Joystick l_Joy = new Joystick();
+        final Joystick r_Joy = new Joystick();
+        final Operator operator = new Operator(in, l_Joy, r_Joy);
+        
+        while(operator.takeAction(time)) {
+            time+=0.1;
+        }
+    }
 }
