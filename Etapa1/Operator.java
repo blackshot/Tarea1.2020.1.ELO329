db70ee083f6a616446e7c18d365686113e398823
Nicolas Aguayo Salas <nicolas.aguayo.13@sansano.usm.cl>
2020-05-17 16:26:40 -0400

    Merge of Stage 1

--- Etapa1/Operator.java
+++ Etapa1/Operator.java
@@ -1,6 +1,12 @@
 import java.util.Scanner;
 
 public class Operator {
+   // FIELDS
+   private float t;
+   private Scanner inFile;
+   private Joystick l_Joystick, r_Joystick;
+
+   // Constructor Method
    public Operator (Scanner in, Joystick l_Joy, Joystick r_Joy){
       inFile = in;
       l_Joystick = l_Joy;
@@ -8,14 +14,39 @@ public class Operator {
       inFile.nextLine(); // skip description line
       t = inFile.nextFloat();
    }
+
+   // Methods
+   /** 
+    * Toma las acciones pertinentes dependiendo del archivo 
+    * de entrada.
+    * @param time (float): el tiempo actual
+    * @return boolean: si se tomo o no la accion
+    */
    public boolean takeAction(float time){
-      float f;
-      if (time > t) {
-        // to be coded.
-       }
-      return true;
+      float ver,rot,forw,side;
+      // If there's data to read
+      if (inFile.hasNextFloat()){
+         // Round to avoid float epsilon difference
+         if (Math.round(time * 10) >= Math.round(t*10)){
+            rot = inFile.nextFloat();
+            ver = inFile.nextFloat();
+            side = inFile.nextFloat();
+            forw = inFile.nextFloat();
+
+            // Left Joystick Data
+            l_Joystick.setHorPos(rot);
+            l_Joystick.setVerPos(ver);
+             // Right Joystick Data
+            r_Joystick.setHorPos(side);
+            r_Joystick.setVerPos(forw);
+
+             // If there's a new line
+            if (inFile.hasNextLine()){
+               t = inFile.nextFloat();
+            }
+         }
+         return true;
+      }
+      else return false;
    }
-   private float t;
-   private Scanner inFile;
-   private Joystick l_Joystick, r_Joystick;
 }
--- Etapa1/Stage1Test.java
+++ Etapa1/Stage1Test.java
@@ -6,12 +6,15 @@ public class Stage1Test  {
    public static void main (String[] arg) throws IOException {
       Locale.setDefault(Locale.US);  // to read number in US format, like 1.5 (not like 1,5)
       Scanner in = new Scanner(new File(arg[0]));
+      // Time init
       float time = 0.0f;
       Joystick l_Joy = new Joystick();
       Joystick r_Joy = new Joystick();
       Operator operator = new Operator(in, l_Joy, r_Joy);
+      
+      System.out.println("Time\tJIH\tJIV\tJDH\tJDV");
       while(operator.takeAction(time)) {
-         System.out.println(/* to be coded */);
+         System.out.printf("%.1f,\t%s, %s%n",time,l_Joy.toString(),r_Joy.toString());
          time+=0.1;
       }
    }
